9aeebda8e1a72ded0df7b610c05f43f2cfacb85b
Tobias Raabe <raabe@posteo.de>
2021-08-08 17:29:36 +0200

    Replace versioneer with setuptools-scm.

--- setup.py
+++ setup.py
@@ -1,46 +1,5 @@
-from pathlib import Path
-
-from setuptools import find_packages
 from setuptools import setup
 
-import versioneer
 
-README = Path("README.rst").read_text()
-
-PROJECT_URLS = {
-    "Documentation": "https://github.com/pytask-dev/latex-dependency-scanner",
-    "Github": "https://github.com/pytask-dev/latex-dependency-scanner",
-    "Tracker": "https://github.com/pytask-dev/latex-dependency-scanner/issues",
-    "Changelog": "https://github.com/pytask-dev/latex-dependency-scanner/blob/main/"
-    "CHANGES.rst",
-}
-
-
-setup(
-    name="latex-dependency-scanner",
-    version=versioneer.get_version(),
-    cmdclass=versioneer.get_cmdclass(),
-    description="Scan LaTeX documents for their dependencies.",
-    long_description=README,
-    long_description_content_type="text/x-rst",
-    author="Tobias Raabe",
-    author_email="raabe@posteo.de",
-    python_requires=">=3.6",
-    url=PROJECT_URLS["Github"],
-    project_urls=PROJECT_URLS,
-    license="MIT",
-    classifiers=[
-        "Development Status :: 3 - Alpha",
-        "Operating System :: OS Independent",
-        "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3 :: Only",
-        "Programming Language :: Python :: 3.6",
-        "Programming Language :: Python :: 3.7",
-        "Programming Language :: Python :: 3.8",
-        "Programming Language :: Python :: 3.9",
-    ],
-    platforms="any",
-    packages=find_packages(where="src"),
-    package_dir={"": "src"},
-    zip_safe=False,
-)
+if __name__ == "__main__":
+    setup()
--- src/latex_dependency_scanner/__init__.py
+++ src/latex_dependency_scanner/__init__.py
@@ -2,10 +2,12 @@
 from latex_dependency_scanner.compile import compile_pdf
 from latex_dependency_scanner.scanner import scan
 
-from ._version import get_versions
+try:
+    from ._version import version as __version__
+except ImportError:
+    # broken installation, we don't even try unknown only works because we do poor mans
+    # version compare
+    __version__ = "unknown"
 
 
-__all__ = ["compile_pdf", "scan"]
-
-__version__ = get_versions()["version"]
-del get_versions
+__all__ = ["__version__", "compile_pdf", "scan"]
